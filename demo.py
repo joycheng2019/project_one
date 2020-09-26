9e4f040b64d036b2eccf7f0586834e6c347bd2fe
joycheng2019 <joycheng2019@gmail.com>
2020-09-26 09:36:11 -0700

    analysis two

--- demo.py
+++ demo.py
@@ -5,3 +5,6 @@ print("This is the pre steps. ")
 print("added analysis features.")
 #added additional cleaning steps
 print("this is some changes to the cleaning steps.")
+
+#added additional analysis two
+print("this is analysis section two.")
